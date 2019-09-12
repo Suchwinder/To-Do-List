ae6d636828ad317fc852e96d9cc9fb47a1c8eb77
Suchwinder Singh <windersingh80@yahoo.com>
2019-09-12 15:17:00 -0400

    Fixed main.js file
    Corrected some syntax errors to allow cross symbol to be displayed

--- JS/main.js
+++ JS/main.js
@@ -1,27 +1,36 @@
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+close.onclick = function() {
+    console.log("trying to close the mainframe")
+    this.parentElement.style.display = "none";
+}
 
-//function that adds a new element, and 
+//function that adds a new list item when Add is clicked
 function newElement(){
     var inputValue = document.getElementById("input").value;
     //checking if client has entered a value
     if(inputValue === ''){
         alert("Please Enter a Task");
     }
-    
     //if there is a value create a list object to store it in and append to un-ordered list
-    var task = document.newElement("li");
+    var task = document.createElement("li");
     var text = document.createTextNode(inputValue);
     task.appendChild(text);
-    document.getElementById("tasks").appendChild(tassk);
+    document.getElementById("tasks").appendChild(task);
     //The item is now appended, need to add close option
-    var cross = document.newElement("SPAN"); //it is SPAN so it is laid to the right/next to the task added
+    var cross = document.createElement("SPAN"); //it is SPAN so it is laid to the right/next to the task added
     var deleteMark = document.createTextNode("\u00D7") //generates cross symbol next to task
     //creating a class for the cross symbol and appending the cross symbol to the SPAN element
-    cross.className("close");
+    cross.className = "close";
     cross.appendChild(deleteMark);
+    task.appendChild(cross);
     
     for(i=0; i < close.length; i++){
-        close[i].onClick() = function() {
-            this.parentElement.display = "none";
+        close[i].onClick = function() {
+            console.log("closing the mainframe")
+            var div = this.parentElement;
+            div.style.display = "none";
         } 
     }
 }
+
